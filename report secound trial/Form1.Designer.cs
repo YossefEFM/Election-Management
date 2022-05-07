c67a52ae468e2d99d83d914e48963403c07fb5fc
jana-hany <104131647+jana-hany@users.noreply.github.com>
2022-05-07 22:09:07 +0200

    Add files via upload

---
+++ report secound trial/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace report_secound_trial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-5, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "view Election result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(139, 12);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1421, 443);
+            this.crystalReportViewer1.TabIndex = 2;
+            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(-5, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 308);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1572, 481);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ report secound trial/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace report_secound_trial
+{
+  
+    public partial class Form1 : Form
+    {  string ordb = "Data Source =orcl; User Id = hr ;Password= hr;";
+    OracleConnection conn;
+        CrystalReport1 cr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {/*
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select candName from  votingvalues";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            listBox1.Items.Add("The Candidates Names and the Total No Of Votes :\n\n");
+            while (dr.Read())
+            {
+
+                listBox1.Items.Add(dr[0] );
+                listBox1.Items.Add("\n");
+            }
+            dr.Close();
+            // listBox1.Items.Add("hello");
+
+
+            /* OracleCommand cmd = new OracleCommand();
+             cmd.Connection = conn;
+             cmd.CommandText = @"select max(NumberofVotes)
+                                from :votingvalues";
+             cmd.CommandType = CommandType.Text;*/
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            cr = new CrystalReport1();
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+           /* OracleCommand cmd1 = new OracleCommand();
+            cmd1.Connection = conn;
+            cmd1.CommandText = @"  select  flagVoteConfirmed
+                                   from    votingvalues ";
+            cmd1.CommandType = CommandType.Text;
+            OracleDataReader dr2 = cmd1.ExecuteReader();
+            while (dr2.Read())
+            {
+               // listBox1.Items.Add(dr2[0]);
+
+            }
+            dr2.Close();
+           */
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = @" select  flagVoteConfirmed
+                                   from  votingvalues ";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            Boolean confirm = false;
+            while (dr.Read())
+            {
+                string value = dr[0].ToString();
+                if (value=="y")
+                {
+                    confirm = true;
+                    crystalReportViewer1.ReportSource = cr;
+                }
+                break;
+
+            }
+            if (confirm == false)
+            {
+                listBox1.Items.Add(" election result ");
+                listBox1.Items.Add(" has not been ");
+                listBox1.Items.Add("confirmed yet!");
+
+            }
+
+
+            // crystalReportViewer1.ReportSource = cr;
+            // listBox1.Items.Add("The Candidates Names and the Total No Of Votes :\n\n");
+
+
+
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
